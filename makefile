c661bc39afbbc5e55e9f51ba3b87299ce359110e
Eduardo Pineda <sepineda@gmail.com>
2025-06-13 13:13:31 -0600

    working with openai realtime api

--- makefile
+++ makefile
@@ -2,3 +2,10 @@
 
 run:
 	uv run app.py
+
+console:
+	uv run textual console
+
+run_dev: 
+	uv run textual run --dev app.py
+
--- src/audio.py
+++ src/audio.py
@@ -1,54 +1,69 @@
-import pyaudio
-import asyncio
-from typing import AsyncIterator
+import threading
+import sounddevice as sd
+import numpy as np
 
-FORMAT = pyaudio.paInt16
 CHANNELS = 1
-RATE = 24000
-CHUNK = 512
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "recordedFile.wav"
+SAMPLE_RATE = 24000
+CHUNK_LENGTH_S = 0.05  # 100ms
 
 
-class AudioStream:
-    def __init__(self, format=FORMAT, channels=CHANNELS, rate=RATE, chunk=CHUNK):
-        self.format = format
-        self.channels = channels
-        self.rate = rate
-        self.chunk = chunk
-        self.p = pyaudio.PyAudio()
-        self.stream = self.p.open(
-            format=self.format,
-            channels=self.channels,
-            rate=self.rate,
-            input=True,
-            frames_per_buffer=self.chunk,
-        )
-        self.paused = True  # Flag to control pause/resume
-
-    async def audio_generator(self) -> AsyncIterator[str]:
-        while True:
-            if not self.paused:
-                data = self.stream.read(self.chunk)
-                yield data
-            await asyncio.sleep(0.001)  # Small delay to avoid busy-waiting
-
-    def pause(self):
-        self.paused = True
-
-    def resume(self):
-        self.paused = False
-
-    def close(self):
-        self.stream.stop_stream()
-        self.stream.close()
-        self.p.terminate()
-
-
-class PlayAudio:
+class AudioPlayerAsync:
     def __init__(self):
-        p = pyaudio.PyAudio()
-        self.stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True)
+        self.queue = []
+        self.lock = threading.Lock()
+        self.stream = sd.OutputStream(
+            callback=self.callback,
+            samplerate=SAMPLE_RATE,
+            channels=CHANNELS,
+            dtype=np.int16,
+            blocksize=int(CHUNK_LENGTH_S * SAMPLE_RATE),
+        )
+        self.playing = False
+        self._frame_count = 0
 
-    def play(self, data):
-        self.stream(data)
+    def callback(self, outdata, frames, time, status):  # noqa
+        with self.lock:
+            data = np.empty(0, dtype=np.int16)
+
+            # get next item from queue if there is still space in the buffer
+            while len(data) < frames and len(self.queue) > 0:
+                item = self.queue.pop(0)
+                frames_needed = frames - len(data)
+                data = np.concatenate((data, item[:frames_needed]))
+                if len(item) > frames_needed:
+                    self.queue.insert(0, item[frames_needed:])
+
+            self._frame_count += len(data)
+
+            # fill the rest of the frames with zeros if there is no more data
+            if len(data) < frames:
+                data = np.concatenate((data, np.zeros(frames - len(data), dtype=np.int16)))
+
+        outdata[:] = data.reshape(-1, 1)
+
+    def reset_frame_count(self):
+        self._frame_count = 0
+
+    def get_frame_count(self):
+        return self._frame_count
+
+    def add_data(self, data: bytes):
+        with self.lock:
+            # bytes is pcm16 single channel audio data, convert to numpy array
+            np_data = np.frombuffer(data, dtype=np.int16)
+            self.queue.append(np_data)
+            if not self.playing:
+                self.start()
+
+    def start(self):
+        self.playing = True
+        self.stream.start()
+
+    def stop(self):
+        self.playing = False
+        self.stream.stop()
+        with self.lock:
+            self.queue = []
+
+    def terminate(self):
+        self.stream.close()
